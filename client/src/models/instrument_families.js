21b78ad1633078e933ed0e7bbeb56f76b9443bba
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-29 07:46:48 +0000

    move erroneous end bracket

--- client/src/models/instrument_families.js
+++ client/src/models/instrument_families.js
@@ -1,21 +1,21 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const InstrumentFamilies = function (data) {
-  this.data = data;
-};
+export default class InstrumentFamilies {
+  constructor (data) {
+    this.data = data;
+  }
 
-InstrumentFamilies.prototype.bindEvents = function () {
+bindEvents ()  {
   PubSub.publish('InstrumentFamilies:data-ready', this.data);
-
   PubSub.subscribe('SelectView:change', (evt) => {
     const selectedIndex = evt.detail;
-    this.publishFamilyDetail(selectedIndex);
+    publishFamilyDetail(selectedIndex);
   });
 };
 
-InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+publishFamilyDetail (selectedIndex) {
   const selectedFamily = this.data[selectedIndex];
   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
 };
 
-module.exports = InstrumentFamilies;
+};
